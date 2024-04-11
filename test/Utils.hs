3c04506a7b48383848c2cc7da773c2358220c3ef
Philipp Joram <mail@phijor.me>
2024-04-11 16:17:58 +0300

    test: Open files in Neovim without creating swap files

--- test/Utils.hs
+++ test/Utils.hs
@@ -104,7 +104,7 @@ vimSpec name secs fp m = do
       withNeovimEmbedded secs $ do
         env <- cornelisInit
         withLocalEnv env $ do
-          vim_command $ "edit " <> T.pack fp'
+          nvim_command $ "noswap edit " <> T.pack fp'
           liftIO $ threadDelay 1e6
           w <- vim_get_current_window
           b <- nvim_win_get_buf w
